9c6508b1f62ce959161484069e9e72e91ed782da
Matthew Cles <matthew@cles.dev>
2020-11-16 15:13:56 +0800

    Convert Tabs to Spaces

--- dot_notation_dictionary.py
+++ dot_notation_dictionary.py
@@ -1,55 +1,55 @@
 
 class DotDict(dict):
-	"""A class that extends 'dict' to allow accessing keys as attributes."""
+    """A class that extends 'dict' to allow accessing keys as attributes."""
 
-	def __init__(self, *args, **kwargs):
-		"""Initalize the DotDict.
-		
-		This method does nothing other that initialize the parent dict 
-		with the passed args and kwargs.
-		"""
-		super(DotDict, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """Initalize the DotDict.
+        
+        This method does nothing other that initialize the parent dict 
+        with the passed args and kwargs.
+        """
+        super(DotDict, self).__init__(*args, **kwargs)
 
-	def __getattr__(self, attribute_name):
-		"""Get the dict value of the key where the attribute_name == key."""
-		try:
-			return self[attribute_name]
-		except AttributeError:
-			raise KeyError(
-				f'KeyError: \'{attribute_name}\''
-			)
+    def __getattr__(self, attribute_name):
+        """Get the dict value of the key where the attribute_name == key."""
+        try:
+            return self[attribute_name]
+        except AttributeError:
+            raise KeyError(
+                f'KeyError: \'{attribute_name}\''
+            )
 
-	def __setattr__(self, attribute_name, attribute_value):
-		"""Set the dict value of the key where the attribute_name == key."""
-		self[attribute_name] = attribute_value
+    def __setattr__(self, attribute_name, attribute_value):
+        """Set the dict value of the key where the attribute_name == key."""
+        self[attribute_name] = attribute_value
 
 class DefaultDotDict(dict):
-	"""A class that extends DotDict to allow default values for keys."""
+    """A class that extends DotDict to allow default values for keys."""
 
-	def __init__(self, default_value=None, *args, **kwargs):
-		"""Initalize the DefaultDotDict.
-		
-		This method sets the default value with the first argument then 
-		initialize the parent dict with the remaining passed args and kwargs.
-		"""
-		super(DefaultDotDict, self).__init__(*args, **kwargs)
-		self.__default_value = default_value
+    def __init__(self, default_value=None, *args, **kwargs):
+        """Initalize the DefaultDotDict.
+        
+        This method sets the default value with the first argument then 
+        initialize the parent dict with the remaining passed args and kwargs.
+        """
+        super(DefaultDotDict, self).__init__(*args, **kwargs)
+        self.__default_value = default_value
 
-	def __getattr__(self, attribute_name):
-		"""Get the dict value of the key where the attribute_name == key."""
-		if '_DefaultDotDict__default_value' == attribute_name:
-			super.__setattr__(self, attribute_name, attribute_value)
-		else:
-			return self[attribute_name]
+    def __getattr__(self, attribute_name):
+        """Get the dict value of the key where the attribute_name == key."""
+        if '_DefaultDotDict__default_value' == attribute_name:
+            super.__setattr__(self, attribute_name, attribute_value)
+        else:
+            return self[attribute_name]
 
-	def __setattr__(self, attribute_name, attribute_value):
-		"""Set the dict value of the key where the attribute_name == key."""
-		if '_DefaultDotDict__default_value' == attribute_name:
-			super.__setattr__(self, attribute_name, attribute_value)
-		else:
-			self[attribute_name] = attribute_value
+    def __setattr__(self, attribute_name, attribute_value):
+        """Set the dict value of the key where the attribute_name == key."""
+        if '_DefaultDotDict__default_value' == attribute_name:
+            super.__setattr__(self, attribute_name, attribute_value)
+        else:
+            self[attribute_name] = attribute_value
 
-	def __missing__(self, key):
-		"""Set the missing key to the default value if it does not exist."""
-		self[key] = self.__default_value
-		return self[key]
+    def __missing__(self, key):
+        """Set the missing key to the default value if it does not exist."""
+        self[key] = self.__default_value
+        return self[key]
