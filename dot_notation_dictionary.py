1a28615e6166be87e2ec1321df06c3ff72e140ef
Matthew Cles <matthew@cles.dev>
2020-11-18 18:40:19 +0800

    Add NestingDotDict class

--- dot_notation_dictionary.py
+++ dot_notation_dictionary.py
@@ -55,3 +55,46 @@ class DefaultDotDict(dict):
         """Set the missing key to the default value if it does not exist."""
         self[key] = self.__default_value
         return self[key]
+
+class NestingDotDict(dict):
+    """A class that extends dict to allow accessing keys as attributes.
+    
+    This class automatically converts and dict class to NestingDotDicts 
+    when accessed from this class. 
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initalize the NestingDotDict.
+        
+        This method does nothing other that initialize the parent dict 
+        with the passed args and kwargs.
+        """
+        super(NestingDotDict, self).__init__(*args, **kwargs)
+
+    def __getattr__(self, attribute_name):
+        """Get the dict value of the key where the attribute_name == key."""
+        try:
+            value = self[attribute_name]
+            if (
+                not isinstance(value,   NestingDotDict) and
+                isinstance(value, dict)
+            ):
+                # If the attribute is a dict, but not already a 
+                # NestingDotDict, then convert it to a NestingDotDict 
+                # and override the current attribute.
+                value = NestingDotDict(value)
+                self[attribute_name] = value
+            return value
+        except AttributeError:
+            raise KeyError(
+                f'KeyError: \'{attribute_name}\''
+            )
+
+    def __setattr__(self, attribute_name, attribute_value):
+        """Set the dict value of the key where the attribute_name == key."""
+        if (
+            not isinstance(attribute_value,   NestingDotDict) and
+            isinstance(attribute_value, dict)
+        ):
+            attribute_value = NestingDotDict(attribute_value)
+        self[attribute_name] = attribute_value
